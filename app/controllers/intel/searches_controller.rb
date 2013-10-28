3fdcf01562b8608a6c5666c12d1d4dec657a1456
Andrew Kane <acekane1@gmail.com>
2013-10-28 01:38:50 -0700

    Updated styles

--- app/controllers/intel/searches_controller.rb
+++ app/controllers/intel/searches_controller.rb
@@ -19,8 +19,8 @@ module Intel
       relation = Intel::Search.where(search_type: params[:search_type])
       @searches_by_week = relation.group_by_week(:created_at, Time.zone, @time_range).count
       @conversions_by_week = relation.where("converted_at is not null").group_by_week(:created_at, Time.zone, @time_range).count
-      @top_searches = @searches.first(10)
-      @bad_conversion_rate = @searches.sort_by{|s| [s["conversion_rate"].to_f, s["query"]] }.first(10).select{|s| s["conversion_rate"] < 50 }
+      @top_searches = @searches.first(5)
+      @bad_conversion_rate = @searches.sort_by{|s| [s["conversion_rate"].to_f, s["query"]] }.first(5).select{|s| s["conversion_rate"] < 50 }
       @conversion_rate_by_week = {}
       @searches_by_week.each do |week, searches_count|
         @conversion_rate_by_week[week] = searches_count > 0 ? (100.0 * @conversions_by_week[week] / searches_count).round : 0
