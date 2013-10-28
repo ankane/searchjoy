3640dd8bcb12fc08a0c932f75573b399815068d2
Andrew Kane <acekane1@gmail.com>
2013-10-27 22:47:17 -0700

    Cleaner code

--- app/controllers/intel/searches_controller.rb
+++ app/controllers/intel/searches_controller.rb
@@ -4,6 +4,7 @@ module Intel
 
     http_basic_authenticate_with name: ENV["INTEL_USERNAME"], password: ENV["INTEL_PASSWORD"] if ENV["INTEL_PASSWORD"]
 
+    before_filter :check_data, only: [:index, :overview, :stream]
     before_filter :set_time_range, only: [:index, :overview]
     before_filter :set_searches, only: [:index, :overview]
 
@@ -14,15 +15,10 @@ module Intel
     end
 
     def overview
-      begin
-        @time_range = 12.weeks.ago.beginning_of_week(:sunday)..Time.now
-        relation = Intel::Search.where(search_type: params[:search_type])
-        @searches_by_week = relation.group_by_week(:created_at, Time.zone, @time_range).count
-        @top_searches = @searches.first(10)
-        @bad_conversion_rate = @searches.sort_by{|s| [s["conversion_rate"].to_f, s["query"]] }.first(10).select{|s| s["conversion_rate"] < 50 }
-      rescue ActiveRecord::StatementInvalid # TODO more selective rescue
-        render text: "Be sure to run rails generate intel:install"
-      end
+      @time_range = 12.weeks.ago.beginning_of_week(:sunday)..Time.now
+      @searches_by_week = Intel::Search.where(search_type: params[:search_type]).group_by_week(:created_at, Time.zone, @time_range).count
+      @top_searches = @searches.first(10)
+      @bad_conversion_rate = @searches.sort_by{|s| [s["conversion_rate"].to_f, s["query"]] }.first(10).select{|s| s["conversion_rate"] < 50 }
     end
 
     def stream
@@ -34,6 +30,16 @@ module Intel
     end
 
     protected
+
+    def check_data
+      begin
+        if Intel::Search.count == 0
+          render "no_data"
+        end
+      rescue ActiveRecord::StatementInvalid
+        render text: "Be sure to run rails generate intel:install"
+      end
+    end
 
     def set_time_range
       @time_range = 12.weeks.ago.beginning_of_week(:sunday)..Time.now
