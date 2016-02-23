e6fb072fb6fd6fa1abf5e747ec5cb0f9c3b23980
Chris Korhonen <ckorhonen@gmail.com>
2016-02-23 16:00:45 -0500

    Support modified queries

--- lib/searchjoy/track.rb
+++ lib/searchjoy/track.rb
@@ -1,13 +1,14 @@
 module Searchjoy
   module Track
-    def search_with_track(term, options = {})
-      results = search_without_track(term, options)
+    def search_with_track(term, options = {}, &block)
+      results = search_without_track(term, options) do |body|
+        block.call(body) if block
+      end
 
       if options[:track]
         attributes = options[:track] == true ? {} : options[:track]
         results.search = Searchjoy::Search.create({search_type: name, query: term, results_count: results.total_count}.merge(attributes))
       end
-
       results
     end
   end
