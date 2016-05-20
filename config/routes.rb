8f660639ed8c13bba09ac310377aea6a26405628
Rodrigo Vitulli <rovitulli@gmail.com>
2016-05-20 18:42:25 -0300

    let the user choose the date range with a datepicker.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Searchjoy::Engine.routes.draw do
   resources :searches, only: [:index] do
     get "overview", on: :collection
   end
+  post "searches/overview"
+  post "searches/index"
   get "searches/recent"
   root to: "searches#stream"
 end
