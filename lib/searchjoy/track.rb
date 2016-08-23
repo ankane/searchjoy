db4f89aafad751ef584487ef905479cd03d395d7
Patrick Nitschke <patrick.nits@googlemail.com>
2016-08-23 08:11:51 +0200

    Updates PR for issue #26
    - use options[:index_name] for search_type if it is stringish
    - use 'All indicies' as default for search_type

--- lib/searchjoy/track.rb
+++ lib/searchjoy/track.rb
@@ -8,10 +8,10 @@ module Searchjoy
         attributes = options[:track] == true ? {} : options[:track]
         search_type = if klass.respond_to?(:name) && klass.name.present?
                         klass.name
-                      elsif options[:index_name].is_a? Array
-                        options[:index_name].join(' ')
+                      elsif options[:index_name]
+                        Array(options[:index_name]).join(', ')
                       else
-                        nil
+                        'All indices'
                       end
         results.search = Searchjoy::Search.create({search_type: search_type, query: term, results_count: results.total_count}.merge(attributes))
       end
