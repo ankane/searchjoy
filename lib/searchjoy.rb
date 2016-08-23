a1513442760818a17afc8087ea77d28363a87800
patrick-nits <patrick.nits@gmail.com>
2016-08-23 10:42:17 +0200

    (#26) Fix tracking of unexecuted searchkick queries and tracking of queries across multiple indices (#27)
    * Fixes tracking in combination with searchkick, when searchkick queries are not executed immediately (see option execute:false).
    
    * Fixes sorting of searches without search_type. This occurs when a search across multiple index names is performed.
    
    * Updates PR for issue #26
    
    - fixes fetch of search_types in SearchesController
    - removes unnecessary hook in Reindex module, removes search_with(out)_track
    - fixes setting search_type in Track
    
    * Updates PR for issue #26
    
    - removes whitespace
    
    * Updates PR for issue #26
    
    - use options[:index_name] for search_type if it is stringish
    - use 'All indicies' as default for search_type

--- lib/searchjoy.rb
+++ lib/searchjoy.rb
@@ -29,13 +29,10 @@ end
 
 if defined?(Searchkick)
   module Searchkick
-    module Reindex
-      def self.extended(base)
-        base.send(:extend, Searchjoy::Track)
-        method_name = Searchkick.respond_to?(:search_method_name) ? Searchkick.search_method_name : :search
-        base.define_singleton_method(:search_without_track, base.method(method_name))
-        base.define_singleton_method(method_name, base.method(:search_with_track))
-      end
+    class Query
+      include Searchjoy::Track
+      define_method(:execute_without_track, instance_method(:execute))
+      define_method(:execute, instance_method(:execute_with_track))
     end
 
     class Results
--- lib/searchjoy/track.rb
+++ lib/searchjoy/track.rb
@@ -1,15 +1,22 @@
 module Searchjoy
   module Track
-    def search_with_track(term, options = {}, &block)
-      results = search_without_track(term, options) do |body|
-        block.call(body) if block
-      end
+
+    def execute_with_track
+      results = execute_without_track
 
       if options[:track]
         attributes = options[:track] == true ? {} : options[:track]
-        results.search = Searchjoy::Search.create({search_type: name, query: term, results_count: results.total_count}.merge(attributes))
+        search_type = if klass.respond_to?(:name) && klass.name.present?
+                        klass.name
+                      elsif options[:index_name]
+                        Array(options[:index_name]).join(', ')
+                      else
+                        'All indices'
+                      end
+        results.search = Searchjoy::Search.create({search_type: search_type, query: term, results_count: results.total_count}.merge(attributes))
       end
       results
     end
+
   end
 end
