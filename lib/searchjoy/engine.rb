451ac58ed2b6d221cd631344120a3e1492a51943
Andrew Kane <andrew@chartkick.com>
2019-10-08 16:25:34 -0700

    Added support for Sprockets 4 - fixes #40

--- lib/searchjoy/engine.rb
+++ lib/searchjoy/engine.rb
@@ -3,8 +3,14 @@ module Searchjoy
     isolate_namespace Searchjoy
 
     initializer "searchjoy" do |app|
-      # use a proc instead of a string
-      app.config.assets.precompile << proc { |path| path == "searchjoy.js" }
+      if app.config.respond_to?(:assets)
+        if defined?(Sprockets) && Sprockets::VERSION >= "4"
+          app.config.assets.precompile << "searchjoy.js"
+        else
+          # use a proc instead of a string
+          app.config.assets.precompile << proc { |path| path == "searchjoy.js" }
+        end
+      end
 
       Searchjoy.attach_to_searchkick! if defined?(Searchkick)
     end
