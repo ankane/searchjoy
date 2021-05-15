532410389e582e78b79e1f86fea23bd005f557a6
Andrew Kane <andrew@ankane.org>
2021-05-14 17:27:44 -0700

    Use Schema.define [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,27 +10,29 @@ ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
 ActiveRecord::Migration.verbose = ENV["VERBOSE"]
 
-ActiveRecord::Migration.create_table :products do |t|
-  t.string :name
-end
+ActiveRecord::Schema.define do
+  create_table :products do |t|
+    t.string :name
+  end
 
-ActiveRecord::Migration.create_table :stores do |t|
-  t.string :name
-end
+  create_table :stores do |t|
+    t.string :name
+  end
 
-ActiveRecord::Migration.create_table :users do |t|
-end
+  create_table :users do |t|
+  end
 
-ActiveRecord::Migration.create_table :searchjoy_searches do |t|
-  t.references :user
-  t.string :search_type
-  t.string :query
-  t.string :normalized_query
-  t.integer :results_count
-  t.datetime :created_at
-  t.references :convertable, polymorphic: true, index: {name: "index_searchjoy_searches_on_convertable"}
-  t.datetime :converted_at
-  t.string :source
+  create_table :searchjoy_searches do |t|
+    t.references :user
+    t.string :search_type
+    t.string :query
+    t.string :normalized_query
+    t.integer :results_count
+    t.datetime :created_at
+    t.references :convertable, polymorphic: true, index: {name: "index_searchjoy_searches_on_convertable"}
+    t.datetime :converted_at
+    t.string :source
+  end
 end
 
 class Product < ActiveRecord::Base
