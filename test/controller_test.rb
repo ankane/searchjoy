ad5cf44572882ae723a5a59c3a4e11b4f1109d18
Andrew Kane <andrew@ankane.org>
2022-03-13 12:44:53 -0700

    Improved controller tests

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -4,5 +4,24 @@ class ControllerTest < ActionDispatch::IntegrationTest
   def test_root
     get searchjoy.root_path
     assert_response :success
+    assert_match "<h1>Live Stream</h1>", response.body
+  end
+
+  def test_overview
+    get searchjoy.overview_searches_path(search_type: "Item")
+    assert_response :success
+    assert_match "Item Overview", response.body
+  end
+
+  def test_top_searches
+    get searchjoy.searches_path(search_type: "Item")
+    assert_response :success
+    assert_match "Item Searches", response.body
+  end
+
+  def test_low_conversions
+    get searchjoy.searches_path(search_type: "Item", sort: "conversion_rate")
+    assert_response :success
+    assert_match "Item Searches", response.body
   end
 end
