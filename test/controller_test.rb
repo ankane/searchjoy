46f9633e923bf81a96b2a53630e126fca4dd7385
Andrew Kane <andrew@ankane.org>
2022-03-13 12:50:54 -0700

    Improved controller tests [skip ci]

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -1,6 +1,16 @@
 require_relative "test_helper"
 
 class ControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    Searchjoy::Search.delete_all
+    Searchjoy::Search.create(
+      search_type: "Item",
+      query: "apple",
+      results_count: 12,
+      user_id: 1
+    )
+  end
+
   def test_root
     get searchjoy.root_path
     assert_response :success
@@ -11,17 +21,20 @@ class ControllerTest < ActionDispatch::IntegrationTest
     get searchjoy.overview_searches_path(search_type: "Item")
     assert_response :success
     assert_match "Item Overview", response.body
+    assert_match "apple", response.body
   end
 
   def test_top_searches
     get searchjoy.searches_path(search_type: "Item")
     assert_response :success
     assert_match "Item Searches", response.body
+    assert_match "apple", response.body
   end
 
   def test_low_conversions
     get searchjoy.searches_path(search_type: "Item", sort: "conversion_rate")
     assert_response :success
     assert_match "Item Searches", response.body
+    assert_match "apple", response.body
   end
 end
